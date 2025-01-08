1dfc6ee16890d05f6a6805b16685bb6a01d94dc7
Nishant Bhardwaj <nishantbj2020@gmail.com>
2025-01-08 23:34:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-const n = 10;
+import { playNote } from "./sound.js";
+let playButton = document.querySelector(".play");
+let initButton = document.querySelector(".init");
+const n = 15;
 let a = [n];
+init();
 
-initialize();
-let audioContext = null;
-
-function playNote(frequency) {
-  if (audioContext == null) {
-    audioContext = new (AudioContext || webLitAudioContext || window.webLitAudioContext)();
-  }
-  
-  const duration = 0.1;
-  const oscillator = audioContext.createOscillator();
-  oscillator.frequency.value = frequency;
-  oscillator.start();
-  oscillator.stop(audioContext.currentTime + duration);
-  
-  const node = audioContext.createGain();
-  node.gain.value = 0.1;
-  node.gain.linearRampToValueAtTime(0, audioContext.currentTime + duration);
-  
-  oscillator.connect(node);
-  node.connect(audioContext.destination);
-}
-
-function initialize() {
+function init() {
   for (let i = 0; i < n; i++) {
-    a[i] = Math.random() * 50;
+    a[i] = Math.floor((Math.random()) *60 );
   }
   console.log(a);
+
   showBars();
 }
 
@@ -38,38 +21,34 @@ function play() {
 }
 
 function animate(moves) {
-  if (moves.length === 0) {
+  if (moves.length == 0) {
     showBars();
     return;
   }
-  
   const move = moves.shift();
   const [i, j] = move.indices;
-  
-  if (move.type === 'swap') {
+  if (move.type == "swap") {
     [a[i], a[j]] = [a[j], a[i]];
   }
-  
+
   playNote(100 + a[i] * 400);
   playNote(100 + a[j] * 400);
-  
+
   showBars(move);
-  
-  setTimeout(function() {
+  setTimeout(function () {
     animate(moves);
   }, 100);
 }
+/// bubble sort
 
-// Bubble Sort
 function sort(a) {
   const moves = [];
-  
   for (let i = 0; i < a.length; i++) {
-    for (let b = 0; b < a.length - 1; b++) {
+    for (let b = 0; b < a.length; b++) {
       if (a[b] > a[b + 1]) {
         moves.push({
           indices: [b, b + 1],
-          type: 'swap'
+          type: "swap",
         });
         let temp = a[b];
         a[b] = a[b + 1];
@@ -77,28 +56,25 @@ function sort(a) {
       }
     }
   }
-  
+  console.log(a);
   return moves;
 }
 
 function showBars(move) {
-  container.innerHTML = '';
-  
+  container.innerHTML = "";
   for (let i = 0; i < a.length; i++) {
-    const bar = document.createElement('div');
-    bar.style.height = a[i] * 4 + 'px';
-    bar.classList.add('bar');
-    
+    const bar = document.createElement("div");
+    bar.style.height = a[i] * 8 + "px";
+    bar.textContent=
+    bar.classList.add("bar");
+
     if (move && move.indices.includes(i)) {
-      bar.style.backgroundColor = move.type === 'swap' ? '#FFD65A' : '#F93827';
+      bar.style.backgroundColor = move.type == "swap" ? "#FFD65A" : "#F93827";
     }
-    
     container.appendChild(bar);
   }
 }
 
-let sortButton = document.querySelector('.play');
-let initButton = document.querySelector('.init');
 
-sortButton.addEventListener('click', play);
-initButton.addEventListener('click', initialize);
+playButton.addEventListener("click", play);
+initButton.addEventListener("click", init);
